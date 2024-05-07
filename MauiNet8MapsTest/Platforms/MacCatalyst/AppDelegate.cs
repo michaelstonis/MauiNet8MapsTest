714ad7de3efbfc62dbfd661c3b1e6ab48815fca9
Michael Stonis <michael.stonis@eightbot.com>
2024-05-06 21:43:59 -0500

    sample maps app

--- MauiNet8MapsTest/Platforms/MacCatalyst/AppDelegate.cs
+++ MauiNet8MapsTest/Platforms/MacCatalyst/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace MauiNet8MapsTest;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
